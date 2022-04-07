4f2841f30121a7c7a404d149b18639bac460e1b9
Shlomi Fish <shlomif@shlomifish.org>
2022-04-07 05:15:13 +0300

    add code

--- shlomif-business-card/align-svg-elements-in-a-2x3-table.pl
+++ shlomif-business-card/align-svg-elements-in-a-2x3-table.pl
@@ -14,15 +14,17 @@ use autodie;
 
 use Path::Tiny qw/ path tempdir tempfile cwd /;
 
-my $sx = 40;
-my $dx = 70;
-my @x  = map { $sx + $dx * $_ } 0 .. 1;
+my $text = $_;
+my $sx   = 40;
+my $dx   = 70;
+my @x    = map { $sx + $dx * $_ } 0 .. 1;
 @x = map { ($_) x 2 } map { @x } 1 .. 3;
 my $sy = 130;
 my $dy = 20;
 my @y  = map { $sy + $dy * $_ } 0 .. 2;
 @y = map { ($_) x 2 } map { ($_) x 2 } @y;
-s%x="[^"]+"%x="@{[shift@x]}"%g;
-s%y="[^"]+"%y="@{[shift@y]}"%g;
+$text =~ s%x="[^"]+"%x="@{[shift@x]}"%g;
+$text =~ s%y="[^"]+"%y="@{[shift@y]}"%g;
 die if @x;
 die if @y;
+$_ = $text;
